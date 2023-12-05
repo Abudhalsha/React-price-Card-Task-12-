4cd0618214ffd909899c80bdf7746a04982a60b3
Abudhalsha <aabudhalsha142155@gmail.com>
2023-12-04 23:14:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import PriceCard from './Components/PriceCard/PriceCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PriceCard></PriceCard>
+    </div>
+  ); 
+}
+
+export default App;
